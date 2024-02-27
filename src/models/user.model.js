450f052d334451dd2ab22e68c3445b37b11cc9df
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-02-27 09:37:41 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -47,13 +47,43 @@ const userSchema = new Schema({
 },
 {
             timestamps: true
-}
+} 
 )
  userSchema.pre("save", async function (next) {
             if(!this.isModified("password")) return next();
             
             this.password = bcrypt.hash(this.password, 10)
             next()        
- });      
+ });    
+ 
+userSchema.methods.isPasswordCorrect = async function(password){
+            return await bcrypt.compare(password, this.password)
+}
+userSchema.methods.generateAccessToken = function(){
+            Jwt.sign(
+                        {
+                                    _id: this.id,
+                                    email: this.email,
+                                    username: this.username,
+                                    fullname: this.fullname
+                        },
+                        process.env.ACCESS_TOKEN_SECRET,
+                        {
+                                    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+                        }
+            )
+}
+userSchema.methods.generateRefreshToken = function () {
+            return Jwt.sign(
+                        {
+                                    _id: this.id,
+                        },
+                        process.env.REFRESH_TOKEN_SECRET,
+                        {
+                                    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+                        }
+            )
+}
+
 
 export const User = mongoose.model("User", userSchema);
